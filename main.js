bdde64b610bac88ed8da0d874835b7ad4bcea2ce
yassermayati1 <96427979+yassermayati1@users.noreply.github.com>
2021-12-26 04:25:21 -0600

    chaqng

--- main.js
+++ main.js
@@ -164,3 +164,11 @@ window.addEventListener( 'resize', function() {
   
   init();
 } )
+$('#login-button').click(function(){
+  $('#login-button').fadeOut("slow",function(){
+    $("#container").fadeIn();
+    TweenMax.from("#container", .4, { scale: 0, ease:Sine.easeInOut});
+    TweenMax.to("#container", .4, { scale: 1, ease:Sine.easeInOut});
+  });
+});
+
