62b55151a3efe5432204bc5a3f47b5e3847c969c
yassermayati1 <96427979+yassermayati1@users.noreply.github.com>
2021-12-27 10:56:26 -0600

    chaqng

--- main.js
+++ main.js
@@ -172,3 +172,26 @@ $('#login-button').click(function(){
   });
 });
 
+/*
+* Fix sidebar at some point and remove
+* fixed position at content bottom
+*/
+$(window).scroll(function () {
+	var headerHeight = $('.site-header').innerHeight();
+	var contentHeight = $('.site-main').innerHeight();
+	var sidebarHeight = $('.side-navigation').height();
+  var sidebarBottomPos = contentHeight - sidebarHeight; 
+  var trigger = $(window).scrollTop() - headerHeight;
+
+      	if ($(window).scrollTop() >= headerHeight) {
+          	$('.side-navigation').addClass('fixed');
+      	} else {
+          	$('.side-navigation').removeClass('fixed');
+      	}
+
+      	if (trigger >= sidebarBottomPos) {
+          	$('.side-navigation').addClass('bottom');
+      	} else {
+          	$('.side-navigation').removeClass('bottom');
+      	}
+});
